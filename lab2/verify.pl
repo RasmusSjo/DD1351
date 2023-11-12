b616ecc9f31e2407fea890ad4219867cbd48b8f5
RasmusSjo <rasmus.sjoberg15@gmail.com>
2023-11-12 22:45:59 +0100

    Implication elim. rule added and verified, placeholders added

--- lab2/verify.pl
+++ lab2/verify.pl
@@ -31,6 +31,11 @@ verify_proof([Head|Tail], Prems, Previous) :-
 verify_rule([_, Statement, premise], Prems, _) :-
     member(Statement, Prems).
 
+% Assumption rule
+
+
+
+
 % Copy rule
 verify_rule([_, Statement, copy(Row)], _, Previous) :-
     member([Row, Statement, _], Previous).
@@ -53,3 +58,59 @@ verify_rule([_, or(Statement, _), orint1(Row)], _, Previous):-
 
 verify_rule([_, or(_, Statement), orint2(Row)], _, Previous) :-
     member([Row, Statement, _], Previous).
+
+% Or elmination rule
+
+
+
+
+% Implication introduction rule
+
+
+
+
+% Implication elimination rule
+verify_rule([_, Statement2, impel(Row1, Row2)], _, Previous) :-
+    member([Row1, Statement1, _], Previous),
+    member([Row2, imp(Statement1, Statement2), _], Previous).
+
+% Negation introduction rule
+
+
+
+
+% Negation elimination rule
+
+
+
+
+% Contradiction elimination
+
+
+
+
+% Negation negation introduction
+
+
+
+
+% Negation negation elimination
+
+
+
+
+% MT rule
+
+
+
+
+% PBC rule
+
+
+
+
+% LEM rule
+
+
+
+
