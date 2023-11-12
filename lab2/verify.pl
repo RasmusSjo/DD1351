0bd76ad028e256119df9db085157f4aeac6960e7
RasmusSjo <rasmus.sjoberg15@gmail.com>
2023-11-12 21:25:23 +0100

    Copy rule added and verified

--- lab2/verify.pl
+++ lab2/verify.pl
@@ -21,12 +21,20 @@ last_line([Statement], Statement).
 last_line([_|Tail], Next_line) :-
     last_line(Tail, Next_line).
 
+% Proof iterator
 verify_proof([], _, _).
 verify_proof([Head|Tail], Prems, Previous) :-
     verify_rule(Head, Prems, Previous),
-    verify_proof(Tail, Prems, Previous).
+    verify_proof(Tail, Prems, [Head|Previous]).
 
+% Premise rule
 verify_rule([_, Statement, premise], Prems, _) :-
     member(Statement, Prems).
+
+% Copy rule
+verify_rule([_, Statement, copy(X)], _, Previous) :-
+    member([X, Statement, _], Previous).
+
+
 
 
