e52d7dbf657840d2e248ecf2d9754a05b61ab8dc
RasmusSjo <rasmus.sjoberg15@gmail.com>
2023-11-12 17:52:46 +0100

    Lab 2 directory

---
+++ lab2/verify.pl
@@ -0,0 +1,36 @@
+
+% Read the file
+verify(fileName) :-
+    see(fileName),
+    read(premises),
+    read(goal),
+    read(proof),
+    seen,
+
+    verify_proof().
+
+
+
+
+
+verify_proof()
+
+verify_proof()
+
+verify_proof()
+
+verify_proof()
+
+verify_proof()
+
+verify_proof()
+
+verify_proof()
+
+verify_proof()
+
+verify_proof()
+
+verify_proof()
+
+
