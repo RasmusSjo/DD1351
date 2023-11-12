41baea1d147589e56d22a805bc963ce731df1547
RasmusSjo <rasmus.sjoberg15@gmail.com>
2023-11-12 21:12:17 +0100

    Added file to verify that premise rule and goal rule are correct

--- lab2/verify.pl
+++ lab2/verify.pl
@@ -7,19 +7,19 @@ verify(FileName) :-
     read(Proof),
     seen,
 
-    valid_proof().
+    valid_proof(Prems, Goal, Proof).
 
 valid_proof(Prems, Goal, Proof) :-
     verify_goal(Proof, Goal) ,
     verify_proof(Proof, Prems, []), !.
 
-last_line([Go], Go).
+verify_goal(Proof, Goal) :-
+    last_line(Proof, Goal_contender),
+    member(Goal, Goal_contender).
+
+last_line([Statement], Statement).
 last_line([_|Tail], Next_line) :-
     last_line(Tail, Next_line).
-
-verify_goal(Proof, Goal) :-
-    last_line(Proof, Goal_con),
-    member(Goal, Goal_con).
 
 verify_proof([], _, _).
 verify_proof([Head|Tail], Prems, Previous) :-
