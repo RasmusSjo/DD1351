41efccb13aaf6c4f422a93166eb196b06b50eab5
RasmusSjo <rasmus.sjoberg15@gmail.com>
2023-12-12 14:07:43 +0100

    and + or implemented + methods for checking all (A) or atleast one (E) adjacent state

--- lab3/verify.pl
+++ lab3/verify.pl
@@ -31,54 +31,72 @@ verify(Input) :-
 
 
 % Literals/atoms (numbers and strings with first character in lower case)
-check(_, L, S, [], Literal) :- 
-    state_variables(S, L, Variables),
+check(_, Labels, State, [], Literal) :- 
+    state_variables(L, State, Variables),
     member(Literal, Variables).
 
-check(_, L, S, [], neg(Literal)) :-
-    state_variables(S, L, Variables),
+check(_, Labels, State, [], neg(Literal)) :-
+    state_variables(L, State, Variables),
     \+ member(Literal, Variables).
 
 
 % And
-check(T, L, S, U, and(F,G)) :-
+check(Transitions, Labels, State, [], and(Statement1, Statement2)) :-
+    check(Transitions, Labels, State, [], Statement1),
+    check(Transitions, Labels, State, [], Statement2).
 
 
 % Or
-check(T, L, S, U, or(F,G)) :-
+check(Transitions, Labels, State, U, or(Statement1, Statement2)) :-
+    check(Transitions, Labels, State, [], Statement1);
+    check(Transitions, Labels, State, [], Statement2).
 
 
 % AX q - q is true in the next state
-check(T, L, S, U, ax(F,G)) :-
+check(Transitions, Labels, State, [], ax(Statement1, Statement2)) :-
+
 
 
 % EX q - q is true in some next state
-check(T, L, S, U, ex(F,G)) :-
+check(Transitions, Labels, State, U, ex(Statement1, Statement2)) :-
 
 
 % AG q - q is true in every state
-check(T, L, S, U, ag(F,G)) :-
+check(Transitions, Labels, State, U, ag(Statement1, Statement2)) :-
 
 
 % EG q - q is true in every state in some path
-check(T, L, S, U, eg(F,G)) :-
+check(Transitions, Labels, State, U, eg(Statement1, Statement2)) :-
 
 
 % AF q - every path will have q true eventually
-check(T, L, S, U, af(F,G)) :-
+check(Transitions, Labels, State, U, af(Statement1, Statement2)) :-
 
 
 % EF q - there is a path where q will eventually be true
-check(T, L, S, U, ef(F,G)) :-
+check(Transitions, Labels, State, U, ef(Statement1, Statement2)) :-
 
 
 
 % Utility methods
 
-% Retrieves the variables that are true for a certain state State
+% Retrieves the variables that are true for a certain state
 state_variables([[State, Variables]|_], State, Variables). % Variables gets unified
 
-state_variables([_|T], S, V) :-
-    state_variables(T, S, V).
+state_variables([_|T], State, Variables) :-
+    state_variables(T, State, Variables).
 
+% Base case, if all adjacent states have been verified or the state doesn't have any transitions (to other states)
+verify_all_adjacents(_, _, [], _, _).
+
+verify_all_adjacents(Transitions, Labels, [Adjacent|Adjacent_states], U, F) :-
+    check(Transitions, Labels, Adjacent, U, F), % Check if the adjacent state is true
+    verify_all_adjacents(Transitions, Labels, Adjacent_states, U, F). % Check if the rest of the adjacent states are true
+
+% Base case, if the state don't have any transitions (to other states)
+verify_all_adjacents(_, _, [], _, _).
+
+verify_atleast_one_adjacent(Transitions, Labels, Adjacent_states, U, F)
+    member(Adjacent, Adjacent_states), % Get every adjacent state
+    check(Transitions, Labels, Adjacent, U, F). % Check every adjacent state til one is true or all are false
 
